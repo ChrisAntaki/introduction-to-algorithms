d3054f006c52d43857ca0af05b19b0ee96ef7b52
Chris Antaki <ChrisAntaki@gmail.com>
2016-08-08 00:56:15 -0700

    You know I am all about that levenshtein cache, dawg

---
+++ strings/levenshtein-distance.js
@@ -0,0 +1,69 @@
+function levenshteinDistance(
+    stringA, lengthA,
+    stringB, lengthB,
+    cache
+) {
+    // Base cases
+    if (lengthA === 0) {
+        return lengthB;
+    }
+
+    if (lengthB === 0) {
+        return lengthA;
+    }
+
+    // Checking cache
+    if (!cache) {
+        cache = {};
+    }
+    var cacheKey = lengthA + ':' + lengthB;
+    var cacheValue = cache[cacheKey];
+    if (cacheValue !== void 0) {
+        return cacheValue;
+    }
+
+    // Core algorithm
+    var cost = 0;
+    if (stringA[lengthA] !== stringB[lengthB]) {
+        cost = 1;
+    }
+
+    var value = Math.min(
+        levenshteinDistance(stringA, lengthA - 1, stringB, lengthB, cache) + 1,
+        levenshteinDistance(stringA, lengthA, stringB, lengthB - 1, cache) + 1,
+        levenshteinDistance(stringA, lengthA - 1, stringB, lengthB - 1, cache) + cost
+    );
+
+    // Set cache
+    cache[cacheKey] = value;
+
+    return value;
+}
+
+function compare(stringA, stringB) {
+    var response = levenshteinDistance(
+        stringA, stringA.length,
+        stringB, stringB.length
+    );
+    console.log(response);
+}
+
+compare(
+    'Hey',
+    'Hay'
+);
+
+compare(
+    'Hello there',
+    'Hallo sire'
+);
+
+compare(
+    'Hello there, good sire, I am inquiring about your breads',
+    'Hallo sire, I am inquiring about your stereo collection'
+);
+
+compare(
+    'You know I am all about that levenshtein cache, dawg!',
+    'You knew I was all about that levenshtein cache, cat.'
+);
