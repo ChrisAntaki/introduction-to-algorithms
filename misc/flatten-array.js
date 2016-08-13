0afc2c35a801672a64695836e9a3012821f63daf
Chris Antaki <ChrisAntaki@gmail.com>
2016-08-13 14:31:02 -0700

    A few more tests for flattenArray

--- misc/flatten-array.js
+++ misc/flatten-array.js
@@ -16,3 +16,8 @@ function flattenArray(list) {
 }
 
 console.log(flattenArray([1,2,[3,4,5],6,[[[[[[[[7], 8], 9]]]]]]]));
+console.log(flattenArray([]));
+console.log(flattenArray([1]));
+console.log(flattenArray([[1]]));
+console.log(flattenArray([[[1]]]));
+console.log(flattenArray([[[1]],2,[3]]));
